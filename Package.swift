34cbbac24ed6b0e5b903f6ba3092fc7c97229da2
Nozhan A <nozhana@gmail.com>
2025-06-12 16:35:23 +0330

    Initial Commit

---
+++ Package.swift
@@ -0,0 +1,21 @@
+// swift-tools-version: 6.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "LiquidPixel",
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "LiquidPixel",
+            targets: ["LiquidPixel"]),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "LiquidPixel"),
+
+    ]
+)
